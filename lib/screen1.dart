026d29fea80d95cd3f91c577fad11403beb9f2ec
RonnyK36 <ronnykelvyne3@gmail.com>
2021-05-31 12:22:21 +0300

    Initial Commit

---
+++ lib/screen1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Screen 1'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // color: Colors.red,
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.red),
+          ),
+          child: Text('Go Forwards To Screen 2'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return Screen2();
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
